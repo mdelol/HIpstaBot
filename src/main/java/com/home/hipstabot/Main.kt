e02b02eb5f61b8f4173e10139078c13b528e2dba
Sergey Kritskiy <sergey.krickij@gmail.com>
2018-05-01 15:48:24 +0700

    moved main to package

--- src/main/java/com/home/hipstabot/Main.kt
+++ src/main/java/com/home/hipstabot/Main.kt
@@ -1,3 +1,5 @@
+package com.home.hipstabot
+
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
